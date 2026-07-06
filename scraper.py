3fc6f32f375f6ad9d5da92bb4c869d002b6fdd98
crigames871-art <crigames871@gmail.com>
2026-07-06 16:54:26 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 # Dán giá trị bạn copy được từ trình duyệt vào đây
-CF_CLEARANCE_TOKEN = "DÁN_GIÁ_TRỊ_BẠN_VỪA_COPY_VÀO_ĐÂY"
+CF_CLEARANCE_TOKEN = "mYYLF70MqUVrULoGsVVzREccYRk5ncux.j9IccnAMPw-1783330709-1.2.1.1-s_hAoAjzhxZK80LLEFNzbbTfhgqaqUAKWjAtFHKBCIn6gFH6mCo88CyBiS.jFca_MTr_1mkQ14IoRgE26hNP3DlI2wNnLZKXNk7Q.i1IdgC2pYNzJW3oVDUESEjsb5V_qh_zeskA4qpFEfSLV3wYCOUCbWpZkv5EdiKcjkH1pZP.cXgOL8AMdtvRkUSjAp7AXVI7eZvsk4G5BzmpknpVe0z953cHQfDzSG7H340RjddyYCLP4vmgUCHzU0mQ18Ilnwf.axHOy4.4OB5FwFWmCLcO57GDBErFR9RV2Ffv2KIqRZrYUavyiqyxKgTCXOBNONZu4gwyj5vWlWCdNTvj.FJMriyEA..E31OfgIq9noe48a7ZQA4Noggxr5kYCOuODakTio3Hhy0DsKqPn5fC81LTk9TmaPTGz_mcgsHwnDYT2hP6Z0LOx2wPHwpx75ZMMzEg18JEepG4MgiQNYGQQSFNGNpid9AovLKBCV9bl1FF6EB1s_KHae7xZhZjbKtc"
 
 api_url = "https://api.tlap17062026.com/matches/graph"
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-    'Cookie': f'cf_clearance=mYYLF70MqUVrULoGsVVzREccYRk5ncux.j9IccnAMPw-1783330709-1.2.1.1-s_hAoAjzhxZK80LLEFNzbbTfhgqaqUAKWjAtFHKBCIn6gFH6mCo88CyBiS.jFca_MTr_1mkQ14IoRgE26hNP3DlI2wNnLZKXNk7Q.i1IdgC2pYNzJW3oVDUESEjsb5V_qh_zeskA4qpFEfSLV3wYCOUCbWpZkv5EdiKcjkH1pZP.cXgOL8AMdtvRkUSjAp7AXVI7eZvsk4G5BzmpknpVe0z953cHQfDzSG7H340RjddyYCLP4vmgUCHzU0mQ18Ilnwf.axHOy4.4OB5FwFWmCLcO57GDBErFR9RV2Ffv2KIqRZrYUavyiqyxKgTCXOBNONZu4gwyj5vWlWCdNTvj.FJMriyEA..E31OfgIq9noe48a7ZQA4Noggxr5kYCOuODakTio3Hhy0DsKqPn5fC81LTk9TmaPTGz_mcgsHwnDYT2hP6Z0LOx2wPHwpx75ZMMzEg18JEepG4MgiQNYGQQSFNGNpid9AovLKBCV9bl1FF6EB1s_KHae7xZhZjbKtc',
+    'Cookie': f'cf_clearance={CF_CLEARANCE_TOKEN}',
     'Origin': 'https://sv1.tieulamwc1.com',
     'Referer': 'https://sv1.tieulamwc1.com/',
     'Content-Type': 'application/json'
